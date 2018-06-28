Fix up analyze_force_timeseries for units consistency and correctly saving np arrays

permeability_functions/thermo_functions.py
@@ -1,7 +1,6 @@
 import itertools
 import numpy as np
 import mdtraj
-import permeability as prm
 import grid_analysis
 import bilayer_analysis_functions
 import simtk.unit as u
@@ -15,15 +14,51 @@ def analyze_force_timeseries(times, forces, meanf_name, fcorr_name,
                             correlation_length=300*u.picosecond):
     """ Given a timeseries of forces, compute force autocorrealtions and means"""
     mean_force = np.mean(forces)
-    dstep = ((times[1] - times[0]) * u.femtosecond).in_units_of(u.picosecond)
-    funlen = int(correlation_length/dstep)._value
-    FACF = prm.acf(forces, None, funlen, dstart=10)
-    time_intervals = np.arange(0, funlen*dstep._value, dstep._value )
-    times_facf = np.column_stack((times,FACF))
+    if isinstance(times, u.Quantity):
+        times = times.in_units_of(u.picosecond)
+    else:
+        times = (times*u.femtosecond).in_units_of(u.picosecond)
+    dstep = times[1] - times[0]
+    funlen = int(correlation_length/dstep)
+    FACF = acf(forces, funlen, dstart=10)
+    time_intervals = np.arange(0, funlen*dstep._value, dstep._value )*dstep.unit
+    times_facf = np.column_stack((time_intervals, FACF))
     np.savetxt(fcorr_name, times_facf)
-    np.savetxt(meanf_name, [mean_force])
+    np.savetxt(meanf_name, [mean_force._value])
 
-    return mean_force, times_facf
+    return mean_force, time_intervals, FACF
+
+def acf(forces, funlen, dstart=10):
+    """Calculate the autocorrelation of a function
+
+    Params
+    ------
+    forces : np.ndarray, shape=(n,)
+        The force timeseries acting on a molecules
+    timestep : float
+        Simulation timestep in fs
+    funlen : int
+        The desired length of the correlation function
+
+    Returns
+    -------
+    corr : np.array, shape=(funlen,)
+        The autocorrelation of the forces
+    """    
+    if funlen > forces.shape[0]:
+       raise Exception("Not enough data")
+    # number of time origins
+    ntraj = int(np.floor((forces.shape[0]-funlen)/dstart))
+    meanfz = np.mean(forces)
+    f1 = np.zeros((funlen)) * meanfz.unit**2
+    origin = 0 
+    for i in range(ntraj):
+        dfzt = (forces[origin:origin+funlen] - meanfz)
+        dfz0 = (forces[origin] - meanfz)
+        f1 += dfzt*dfz0
+        origin += dstart
+    return f1/ntraj
+
 
 def integrate_facf_over_time(times, facf, average_fraction=0.1):
     """ Integrate force autocorelations