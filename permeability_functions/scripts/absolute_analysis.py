b5ad25c4b93e367685afa7ee083877b0b5e964bb
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-08-13 11:00:21 -0500

    extra imports, fix bugs in loop iterating

--- permeability_functions/scripts/absolute_analysis.py
+++ permeability_functions/scripts/absolute_analysis.py
@@ -1,3 +1,4 @@
+import os
 import pdb
 import glob
 import matplotlib
@@ -8,39 +9,44 @@ import permeability_functions.misc as misc
 import numpy as np
 import simtk.unit as u
 def main():
+    all_sweeps = [thing for thing in os.listdir() is os.path.isdir(thing) and 'cache' not in thing]
+    curr_dir = os.getcwd()
     n_sims = 5
-    reaction_coordinates = np.loadtxt('z_windows.out') * u.nanometer
-    n_windows = len(reaction_coordinates)
-    window_forces = np.zeros(n_windows)
-    window_facf_integrals = np.zeros(n_windows)
-    for sim_number in range(n_sims):
-        tracers = np.loadtxt('Sim{0}/tracers.out'.format(sim_number), dtype=int) - 1
-        for i in enumerate(tracers):
-            forceout_id = sim_number + (i*n_sims)
-            data = np.loadtxt('Sim{0}/forceout{1}.dat'.format(sim_number, forceout_id))
-            times = data[:,1] * u.femtosecond
-            forces = data[:,2] * u.kilocalorie/(u.mole*u.angstrom)
-            mean_force , time_intervals, facf = thermo_functions.analyze_force_timeseries(
-                    times, forces, 
-                    meanf_name='Sim{0}/meanforce{1}.dat'.format(sim_number, forceout_id), 
-                    fcorr_name='Sim{0}/fcorr{1}.dat'.format(sim_number, forceout_id))
-            intF, intFval = thermo_functions.integrate_facf_over_time(time_intervals, 
-                                                                        facf)
-            window_forces[forceout_id] = mean_force._value
-            window_facf_integrals[forceout_id] = intFval._value
-    
-    
-    (reaction_coordinates, mean_forces, facf_integrals, fe_profile, 
-                diffusion_profile, resistance_profile, resistance_integral, 
-                permeability_profile, permeability_integral) = thermo_functions.permeability_routine(reaction_coordinates, window_forces, window_facf_integrals)
-    
-    np.savetxt('diffusion_profile.dat', np.column_stack((reaction_coordinates, diffusion_profile)))
-    np.savetxt('free_energy_profile.dat', np.column_stack((reaction_coordinates, fe_profile)))
-    np.savetxt('resistance_profile.dat', np.column_stack((reaction_coordinates, resistance_profile)))
-    np.savetxt('permeability_profile.dat', np.column_stack((reaction_coordinates, permeability_profile)))
-    
-    
-    print(permeability_integral)
+    for sweep in all_sweeps:
+        print(sweep)
+        os.chdir(os.path.join(curr_dir, sweep))
+        reaction_coordinates = np.loadtxt('z_windows.out') * u.nanometer
+        n_windows = len(reaction_coordinates)
+        window_forces = np.zeros(n_windows)
+        window_facf_integrals = np.zeros(n_windows)
+        for sim_number in range(n_sims):
+            tracers = np.loadtxt('Sim{0}/tracers.out'.format(sim_number), dtype=int) - 1
+            for i,tracerid in enumerate(tracers):
+                forceout_id = sim_number + (i*n_sims)
+                data = np.loadtxt('Sim{0}/forceout{1}.dat'.format(sim_number, forceout_id))
+                times = data[:,1] * u.femtosecond
+                forces = data[:,2] * u.kilocalorie/(u.mole*u.angstrom)
+                mean_force , time_intervals, facf = thermo_functions.analyze_force_timeseries(
+                        times, forces, 
+                        meanf_name='Sim{0}/meanforce{1}.dat'.format(sim_number, forceout_id), 
+                        fcorr_name='Sim{0}/fcorr{1}.dat'.format(sim_number, forceout_id))
+                intF, intFval = thermo_functions.integrate_facf_over_time(time_intervals, 
+                                                                            facf)
+                window_forces[forceout_id] = mean_force._value
+                window_facf_integrals[forceout_id] = intFval._value
+        
+        
+        (reaction_coordinates, mean_forces, facf_integrals, fe_profile, 
+                    diffusion_profile, resistance_profile, resistance_integral, 
+                    permeability_profile, permeability_integral) = thermo_functions.permeability_routine(reaction_coordinates, window_forces, window_facf_integrals)
+        
+        np.savetxt('diffusion_profile.dat', np.column_stack((reaction_coordinates, diffusion_profile)))
+        np.savetxt('free_energy_profile.dat', np.column_stack((reaction_coordinates, fe_profile)))
+        np.savetxt('resistance_profile.dat', np.column_stack((reaction_coordinates, resistance_profile)))
+        np.savetxt('permeability_profile.dat', np.column_stack((reaction_coordinates, permeability_profile)))
+        
+        
+        print(permeability_integral)
     
     # Plotting
     fig, ax = plt.subplots(2,1)
